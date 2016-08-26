41840f1ee954583cd4b681a34b2df3804f84816f
dany1468 <dany1468@gmail.com>
2016-08-26 23:10:16 +0900

    Reject ignoring gem from import candidate unread gems

--- app/batches/unread_gems_importer.rb
+++ app/batches/unread_gems_importer.rb
@@ -1,8 +1,18 @@
 class UnreadGemsImporter
   def perform!
+    unread_gems = collect_unread_gems!
+
+    unread_gems.reject! {|gem| gem.name.in?(ignoring_gem_names) }
+  end
+
+  private
+
+  def collect_unread_gems!
+    unread_gems = []
+
     RubyGem.collect_gems(current_cursor) do |ruby_gems, next_cursor|
       ActiveRecord::Base.transaction do
-        ruby_gems.map {|gem|
+        unread_gems = ruby_gems.map {|gem|
           UnreadGem.create(
             name: gem.name,
             version: gem.version,
@@ -14,11 +24,15 @@ class UnreadGemsImporter
         ExecutionHistory.create(latest_tweet_id: next_cursor, start_tweet_id: current_cursor)
       end
     end
-  end
 
-  private
+    unread_gems
+  end
 
   def current_cursor
     @current_cursor ||= ExecutionHistory.last.latest_tweet_id
+  end
+
+  def ignoring_gem_names
+    @ignoring_gem_names = IgnoringGem.all.pluck(:nam)
   end
 end
